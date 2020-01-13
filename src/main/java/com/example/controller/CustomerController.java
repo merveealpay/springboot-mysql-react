7bd4cd7a4d787e9a5c881fc4c3aa83fde8f3431d
merve <merveealpay@gmail.com>
2020-01-13 20:05:58 +0300

    added controller

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,16 +1,49 @@
 package com.example.controller;
 
+import com.example.entity.Customer;
 import com.example.service.ICustomerService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+//http requests from frontend
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/customers")
 @RequiredArgsConstructor
+
 public class CustomerController {
 
+    ICustomerService customerService;
+    public CustomerController(ICustomerService customerService) {
+        this.customerService=customerService;
+    }
 
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public List<Customer> findAll() {
+        return customerService.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Customer findById(@PathVariable  int id) {
+        return customerService.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public void addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        customerService.save(customer);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
+    public void updateCustomer(@RequestBody Customer customer){
+        customerService.save(customer);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, produces = "application/json")
+    public void deleteCustomerById(@PathVariable int id) {
+        customerService.deleteById(id);
+    }
 }
